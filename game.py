d01f370867c800108f1377de4488a6ee0602a33d
isu sharma <ishusharmapaudel@gmail.com>
2024-05-23 10:24:41 +0545

    whack a mole

---
+++ game.py
@@ -0,0 +1,103 @@
+'''
+    There are two versions of same function, one for normal os and one for mac os.
+    Since the library hasn't been fixed yet,
+    we have to divide the center location by 2 and 
+    thus we will get the image location and the rest is same.
+'''
+import pyautogui
+import time
+import os
+
+# size,position,click,moveTo
+
+# Change directory to the script's directory
+os.chdir(os.path.dirname(__file__))
+
+# Paths to images
+moles = ["assets/mole1.png", "assets/mole2.png"]
+START_BUTTON = "assets/start.png"
+CONTINUE_BUTTON = "assets/continue.png"
+
+def click_button(image_path, button_name):
+    # """
+    # Locate and click a button on the screen.
+
+    # Parameters:
+    #     image_path (str): Path to the button image.
+    #     button_name (str): Name of the button for display purposes.
+    # """
+    print(f"Locating {button_name} Button!")
+    try:
+        button_location = pyautogui.locateOnScreen(image_path, confidence=0.8)
+        print(button_location)
+        pyautogui.click(button_location)
+    except pyautogui.ImageNotFoundException:
+        print(f"Error locating {button_name} button!")
+
+# def click_button_mac(image_path, button_name):
+#     """
+#     Locate and click a button on the screen for Mac (Retina Display). Give permission to Vscode and the web-browser for screen recording from settings
+
+#     Parameters:
+#         image_path (str): Path to the button image.
+#         button_name (str): Name of the button for display purposes.
+#     """
+#     print(f"Locating {button_name} Button!")
+#     try:
+#         button_location = pyautogui.locateCenterOnScreen(image_path, confidence=0.8)
+#         print(button_location)
+#         x = button_location.x / 2
+#         y = button_location.y / 2 
+#         pyautogui.click(x,y)
+#     except pyautogui.ImageNotFoundException:
+#         print(f"Error locating {button_name} button!")
+
+
+def whack_mole():
+    """
+    Whack moles on the screen.
+    """
+    for mole_image in moles:
+        try:
+            moles_location = pyautogui.locateOnScreen(mole_image, confidence=0.5,grayscale=True)
+            pyautogui.click(moles_location)
+            print("Whacked!")
+        except pyautogui.ImageNotFoundException:
+            print("No Mole on Screen")
+
+# def whack_mole_mac():
+#     """
+#     Whack moles on the screen for mac_devices.
+#     """
+#     for mole_image in moles:
+#         try:
+#             moles_location = pyautogui.locateCenterOnScreen(mole_image, confidence=0.8)
+#             # x = moles_location.x/2
+#             # y = moles_location.y/2
+#             pyautogui.click(moles_location)
+#             print("Whacked!")
+#         except pyautogui.ImageNotFoundException:
+#             print("No Mole on Screen")
+
+if __name__ == "__main__":
+    time.sleep(5) # 10 seconds of timer to manually open the https://plays.org/whack-a-mole/ site and make it ready with Start Button on Screen
+    click_button(CONTINUE_BUTTON, "Continue")
+    click_button(START_BUTTON, "Start")
+    # click_button_mac(CONTINUE_BUTTON, "Continue")
+    # click_button_mac(START_BUTTON, "Start")
+
+    while True:
+        whack_mole()
+        time.sleep(0.1)  # Small delay to prevent excessive CPU usage
+
+'''
+if __name__ == "__main__": Block:
+
+The if __name__ == "__main__": block is a common idiom in Python scripts.
+It checks whether the current script is being run as the main program or if it's being imported as a module into another script.
+If the script is being run as the main program (i.e., directly executed), the condition __name__ == "__main__" evaluates to True.
+In this case, the code inside the if block is executed. This typically includes the main functionality of the script, such as defining functions, performing operations, etc.
+If the script is being imported as a module into another script, the condition __name__ == "__main__" evaluates to False, and the code inside the if block is not executed.
+
+
+'''
